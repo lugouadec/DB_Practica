6d2560770190a5ef77b78ba4c5faf6dd2de5f216
Dr. Jose Edgar Lugo Castro <lugouadec@gmail.com>
2022-11-18 11:02:09 -0600

    Actualizacion de las Tablas

--- DB_Estructura.sql
+++ DB_Estructura.sql
@@ -1,42 +1,35 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
---
--- Host: localhost    Database: empresa
--- ------------------------------------------------------
--- Server version	8.0.21
+-- --------------------------------------------------------
+-- Host:                         148.212.195.26
+-- Versión del servidor:         8.0.31-0ubuntu0.20.04.1 - (Ubuntu)
+-- SO del servidor:              Linux
+-- HeidiSQL Versión:             12.1.0.6537
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `categoria_salario`
---
 
-DROP TABLE IF EXISTS `categoria_salario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categoria_salario` (
+-- Volcando estructura de base de datos para empresa
+CREATE DATABASE IF NOT EXISTS `empresa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `empresa`;
+
+-- Volcando estructura para tabla empresa.categoria_salario
+CREATE TABLE IF NOT EXISTS `categoria_salario` (
   `no_categoria` int DEFAULT NULL,
   `salario_minimo` decimal(4,0) DEFAULT NULL,
   `salario_maximo` decimal(4,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `clientes`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `clientes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `clientes` (
+-- Volcando estructura para tabla empresa.clientes
+CREATE TABLE IF NOT EXISTS `clientes` (
   `id_clientes` int NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `direccion` varchar(40) DEFAULT NULL,
@@ -50,77 +43,55 @@ CREATE TABLE `clientes` (
   `comentarios` blob,
   PRIMARY KEY (`id_clientes`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `departamento`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `departamento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `departamento` (
+-- Volcando estructura para tabla empresa.departamento
+CREATE TABLE IF NOT EXISTS `departamento` (
   `no_departamento` int NOT NULL,
   `nombre` varchar(14) DEFAULT NULL,
   `ubicacion` varchar(13) DEFAULT NULL,
   PRIMARY KEY (`no_departamento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `detalle_orden`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `detalle_orden`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `detalle_orden` (
-  `id_articulos` int NOT NULL,
+-- Volcando estructura para tabla empresa.detalle_orden
+CREATE TABLE IF NOT EXISTS `detalle_orden` (
+  `id_detalle_orden` int NOT NULL AUTO_INCREMENT,
   `id_orden` int NOT NULL,
   `id_productos` int NOT NULL,
   `precio_actual` decimal(8,2) DEFAULT NULL,
   `cantidad` decimal(8,0) DEFAULT NULL,
   `total_articulos` decimal(8,2) DEFAULT NULL,
-  `id_detalle_orden` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id_detalle_orden`),
   KEY `fk_detalle_orden_productos1_idx` (`id_productos`),
   KEY `fk_detalle_orden_ordenes1` (`id_orden`),
   CONSTRAINT `fk_detalle_orden_ordenes1` FOREIGN KEY (`id_orden`) REFERENCES `ordenes` (`id_orden`),
   CONSTRAINT `fk_detalle_orden_productos1` FOREIGN KEY (`id_productos`) REFERENCES `productos` (`id_producto`)
 ) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `empleados`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `empleados`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `empleados` (
+-- Volcando estructura para tabla empresa.empleados
+CREATE TABLE IF NOT EXISTS `empleados` (
   `no_empleado` int NOT NULL,
   `no_departamento` int NOT NULL,
   `nombre` varchar(30) DEFAULT NULL,
   `puesto` varchar(20) DEFAULT NULL,
   `jefe_directo` int DEFAULT NULL,
-  `fecha_alquiler` date DEFAULT NULL,
+  `f_ingreso` date DEFAULT NULL,
   `salario` decimal(7,2) DEFAULT NULL,
   `comision` decimal(7,2) DEFAULT NULL,
-  `departamento_no_departamento` int NOT NULL,
   PRIMARY KEY (`no_empleado`),
-  KEY `fk_empleados_departamento_idx` (`departamento_no_departamento`),
-  CONSTRAINT `fk_empleados_departamento` FOREIGN KEY (`departamento_no_departamento`) REFERENCES `departamento` (`no_departamento`)
+  KEY `fk_empleados_departamento_idx` (`no_departamento`),
+  CONSTRAINT `fk_empleados_departamento` FOREIGN KEY (`no_departamento`) REFERENCES `departamento` (`no_departamento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `ordenes`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `ordenes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ordenes` (
+-- Volcando estructura para tabla empresa.ordenes
+CREATE TABLE IF NOT EXISTS `ordenes` (
   `id_orden` int NOT NULL,
   `id_clientes` int NOT NULL,
   `fecha_orden` date DEFAULT NULL,
@@ -131,16 +102,11 @@ CREATE TABLE `ordenes` (
   KEY `fk_ordenes_clientes1_idx` (`id_clientes`),
   CONSTRAINT `fk_ordenes_clientes1` FOREIGN KEY (`id_clientes`) REFERENCES `clientes` (`id_clientes`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `precio`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `precio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `precio` (
+-- Volcando estructura para tabla empresa.precio
+CREATE TABLE IF NOT EXISTS `precio` (
   `idPrecio` int NOT NULL AUTO_INCREMENT,
   `id_producto` int NOT NULL,
   `precio_normal` decimal(8,2) DEFAULT NULL,
@@ -151,29 +117,20 @@ CREATE TABLE `precio` (
   KEY `fk_precio_productos1_idx` (`id_producto`),
   CONSTRAINT `fk_precio_productos1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `productos`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `productos` (
+-- Volcando estructura para tabla empresa.productos
+CREATE TABLE IF NOT EXISTS `productos` (
   `id_producto` int NOT NULL,
   `descripcion` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- La exportación de datos fue deseleccionada.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-11-17 22:48:04
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
